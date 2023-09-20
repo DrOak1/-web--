a8c7a90c1944b8bd560010681bd2b4bd39a7ab63
DrOak <144041485+DrOak1@users.noreply.github.com>
2023-09-20 16:58:50 +0300

    Update App.java

--- Laba1/src/main/java/com/iba/project/App.java
+++ Laba1/src/main/java/com/iba/project/App.java
@@ -3,5 +3,6 @@ package com.iba.project;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("New Changes");
     }
 }
